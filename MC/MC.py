d99c90c0b7e5165faf5c19e68314a3caaa9bac22
Viet Pham <vietuet98@gmail.com>
2021-03-03 23:13:57 +0700

    fix runMC 2nd order

--- MC/MC.py
+++ MC/MC.py
@@ -12,13 +12,22 @@ class MarkovChain():
     self.w_behavior = weight_behaivor
     self.transition_matrix = transition_matrix
 
-  def top_predicted_item(self, previous_basket, topk):
+  def top_predicted_item(self, previous_baskets, topk):
     candidate = np.zeros(self.nb_items)
-    prev_basket_idx = [self.item_dict[item] for item in previous_basket]
+    # last_basket = previous_baskets[-1]
+    transition_score = np.zeros(len(previous_baskets[0]))
+    for i in range(1, len(previous_baskets)):
+      prev_score = transition_score
+      prev_basket_idx = [self.item_dict[item] for item in previous_baskets[i-1]]
+      cur_basket_idx = [self.item_dict[item] for item in previous_baskets[i]]
+      transition_score = np.array((prev_score + np.log(self.transition_matrix[prev_basket_idx, cur_basket_idx].todense())).sum(axis=0))[0]
+      transition_score = transition_score / len(previous_baskets[i-1])
+
+    last_basket_idx = [self.item_dict[item] for item in previous_baskets[-1]]
     # for item_idx in prev_basket_idx:
     # for i in range(len(self.behavior_dict)):
-    candidate = np.array(self.transition_matrix[prev_basket_idx, :].todense().sum(axis=0))[0]
-    candidate = candidate / len(prev_basket_idx)
+    candidate = (transition_score + np.array(self.transition_matrix[last_basket_idx, :].todense()).sum(axis=0))[0]
+    candidate = candidate / len(last_basket_idx)
     topk_idx = np.argpartition(candidate, -topk)[-topk:]
     sorted_topk_idx = topk_idx[np.argsort(candidate[topk_idx])]
     topk_item = [self.reversed_item_dict[item] for item in sorted_topk_idx]
